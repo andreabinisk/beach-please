e5e3111cb2946956c6624edae6ef0cc54245b99e
Andrea Biniskiewicz <andreabiniskiewicz@Andreas-MacBook-Pro.local>
2019-06-15 15:49:03 -0700

    button

--- bpScriptFile.js
+++ bpScriptFile.js
@@ -1,4 +1,5 @@
 let button01 = document.getElementById("Get Weather");
+
 fetch(
   "http://api.openweathermap.org/data/2.5/weather?lat=35.1799752&lon=-120.7318418&APPID=8eaf7058bd5a4db9700ee42db10f28bb"
 )
@@ -9,7 +10,7 @@ fetch(
     // this is where you use the data coming in
     let result = [];
     for (let key in myJson) {
-      result.push(myJson[key].main.temp);
+      result.push(myJson[key].main);
     }
   });
 button01.addEventListener("click", fetch, false);
